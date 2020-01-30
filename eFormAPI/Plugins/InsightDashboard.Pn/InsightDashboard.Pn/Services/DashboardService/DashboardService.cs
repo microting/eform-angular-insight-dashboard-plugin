a71d087b56c25a53da8bf2ff26c2f2de102fa0ae
Leonid <leonid.atf@gmail.com>
2020-01-30 17:57:43 +0200

    Updates

--- eFormAPI/Plugins/InsightDashboard.Pn/InsightDashboard.Pn/Services/DashboardService/DashboardService.cs
+++ eFormAPI/Plugins/InsightDashboard.Pn/InsightDashboard.Pn/Services/DashboardService/DashboardService.cs
@@ -727,16 +727,27 @@ namespace InsightDashboard.Pn.Services.DashboardService
                                     {
                                         multiData = data
                                             .GroupBy(x => x.Name)
-                                            .Select(x => new DashboardViewChartDataMultiModel
+                                            .Select(x => new 
                                             {
                                                 Name = x.Key,
-                                                Series = x
+                                                Total = x.Count(),
+                                                Items = x
                                                     .GroupBy(y => $"{y.Finished:yy-MMM}")
-                                                    .Select(y => new DashboardViewChartDataSingleModel
+                                                    .Select(y => new
                                                     {
                                                         Name = y.Key,
                                                         Value = Math.Round(((decimal) y.Count() * 100) / x.Count(), 2),
                                                     }).ToList()
+                                            })
+                                            .Select(x => new DashboardViewChartDataMultiModel
+                                            {
+                                                Name = x.Name,
+                                                Series = x.Items
+                                                    .Select(y => new DashboardViewChartDataSingleModel
+                                                    {
+                                                        Name = y.Name,
+                                                        Value = y.Value,
+                                                    }).ToList(),
                                             })
                                             .ToList();
 
