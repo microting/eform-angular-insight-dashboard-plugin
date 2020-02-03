ac50ecc3d64ac04b4e8ecc845a95a9f1211c4d45
Gid <mkazli733@gmail.com>
2020-02-03 12:27:43 +0200

    Change plugin name

--- eform-client/e2e/Tests/insight-dashboard-settings/application-settings.plugins-page.spec.ts
+++ eform-client/e2e/Tests/insight-dashboard-settings/application-settings.plugins-page.spec.ts
@@ -18,7 +18,7 @@ describe('Application settings page - site header section', function () {
 
     const plugin = pluginsPage.getFirstPluginRowObj();
     expect(plugin.id).equal(1);
-    expect(plugin.name).equal('Microting InsightDashboard Plugin');
+    expect(plugin.name).equal('Microting Insight Dashboard Plugin');
     expect(plugin.version).equal('1.0.0.0');
 
   });
@@ -39,7 +39,7 @@ describe('Application settings page - site header section', function () {
 
     let pluginToFind = pluginsPage.getFirstPluginRowObj();
     expect(pluginToFind.id).equal(1);
-    expect(pluginToFind.name).equal('Microting InsightDashboard Plugin');
+    expect(pluginToFind.name).equal('Microting Insight Dashboard Plugin');
     expect(pluginToFind.version).equal('1.0.0.0');
     expect(browser.element(`//*[contains(text(), 'Insight Instrumentbræt')]`).isExisting()).equal(true);
   });
