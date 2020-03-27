59bdc8b0b723739a32ef449f2abf4404a39f3964
René Schultz Madsen <rm@microting.dk>
2020-03-27 10:05:41 +0100

    Updating the devinstall.sh

--- devinstall.sh
+++ devinstall.sh
@@ -2,9 +2,20 @@
 cd ~
 pwd
 
+
 rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/insight-dashboard-pn
+rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/insight-dashboard-settings
+rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/insight-dashboard-general
+rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Assets
+rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/InsightDashboard
+rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js
 
 cp -av Documents/workspace/microting/eform-angular-insight-dashboard-plugin/eform-client/src/app/plugins/modules/insight-dashboard-pn Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/insight-dashboard-pn
+cp -av Documents/workspace/microting/eform-angular-basecustomer-plugin/eform-client/e2e/Tests/insight-dashboard-settings Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/insight-dashboard-settings
+cp -av Documents/workspace/microting/eform-angular-basecustomer-plugin/eform-client/e2e/Tests/insight-dashboard-general Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/insight-dashboard-general
+cp -av Documents/workspace/microting/eform-angular-basecustomer-plugin/eform-client/e2e/Assets/ Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Assets
+cp -av Documents/workspace/microting/eform-angular-basecustomer-plugin/eform-client/e2e/Page\ objects/InsightDashboard Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/InsightDashboard
+cp -av Documents/workspace/microting/eform-angular-basecustomer-plugin/eform-client/wdio-headless-plugin-step2.conf.js Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js
 
 rm -fR Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/InsightDashboard.Pn
 
