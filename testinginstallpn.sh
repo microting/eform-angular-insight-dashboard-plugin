4968722f20c9f3368c8b257f93b052495d1a5f72
René Schultz Madsen <rm@microting.dk>
2020-03-30 15:07:14 +0200

    Fixing the plugin name.

--- testinginstallpn.sh
+++ testinginstallpn.sh
@@ -2,5 +2,5 @@
 perl -pi -e '$_.="  }\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
 perl -pi -e '$_.="  loadChildren: '\''./modules/insight-dashboard-pn/insight-dashboard-pn.module#InsightDashboardPnModule'\''\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
 perl -pi -e '$_.="  canActivate: [AuthGuard],\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
-perl -pi -e '$_.="  path: '\''trash-inspection-pn'\'',\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
+perl -pi -e '$_.="  path: '\''insight-dashboard-pn'\'',\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
 perl -pi -e '$_.="  {\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
