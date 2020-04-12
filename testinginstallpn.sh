95674eab0c3c515fc8ac4088f8981cf43259bc44
René Schultz Madsen <rm@microting.dk>
2020-04-12 11:37:35 +0200

    Adding the missing ,

--- testinginstallpn.sh
+++ testinginstallpn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-perl -pi -e '$_.="  }\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
+perl -pi -e '$_.="  },\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
 perl -pi -e '$_.="  loadChildren: '\''./modules/insight-dashboard-pn/insight-dashboard-pn.module#InsightDashboardPnModule'\''\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
 perl -pi -e '$_.="  canActivate: [AuthGuard],\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
 perl -pi -e '$_.="  path: '\''insight-dashboard-pn'\'',\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
