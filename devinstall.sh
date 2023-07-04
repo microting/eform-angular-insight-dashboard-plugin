3908ef854c7d216512e121663a7a87ed931fde61
René Schultz Madsen <rm@microting.dk>
2023-07-04 19:30:48 +0200

    Splitting the tests into a,b,c to make it easier to rerun tests that failed due to high load on github actions.

--- devinstall.sh
+++ devinstall.sh
@@ -7,14 +7,18 @@ rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/src/app
 rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/insight-dashboard-settings
 rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/insight-dashboard-general
 rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/InsightDashboard
-rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.ts
+rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2a.conf.ts
+rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2b.conf.ts
+rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2c.conf.ts
 mkdir -p Documents/workspace/microting/eform-angular-frontend/eform-client/cypress/e2e/plugins
 
 cp -av Documents/workspace/microting/eform-angular-insight-dashboard-plugin/eform-client/src/app/plugins/modules/insight-dashboard-pn Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/insight-dashboard-pn
 cp -av Documents/workspace/microting/eform-angular-insight-dashboard-plugin/eform-client/e2e/Tests/insight-dashboard-settings Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/insight-dashboard-settings
 cp -av Documents/workspace/microting/eform-angular-insight-dashboard-plugin/eform-client/e2e/Tests/insight-dashboard-general Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/insight-dashboard-general
 cp -av Documents/workspace/microting/eform-angular-insight-dashboard-plugin/eform-client/e2e/Page\ objects/InsightDashboard Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/InsightDashboard
-cp -av Documents/workspace/microting/eform-angular-insight-dashboard-plugin/eform-client/wdio-headless-plugin-step2.conf.ts Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.ts
+cp -av Documents/workspace/microting/eform-angular-insight-dashboard-plugin/eform-client/wdio-headless-plugin-step2a.conf.ts Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2a.conf.ts
+cp -av Documents/workspace/microting/eform-angular-insight-dashboard-plugin/eform-client/wdio-headless-plugin-step2b.conf.ts Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2b.conf.ts
+cp -av Documents/workspace/microting/eform-angular-insight-dashboard-plugin/eform-client/wdio-headless-plugin-step2c.conf.ts Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2c.conf.ts
 
 rm -fR Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/InsightDashboard.Pn
 
