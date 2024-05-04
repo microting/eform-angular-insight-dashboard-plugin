d3b352fe93d5b5498f8bdc2c1f2ee84854022df2
René Schultz Madsen <rm@microting.dk>
2024-05-04 06:36:12 +0200

    Fixing create dashboard modal.

--- eform-client/e2e/Tests/insight-dashboard-general/dashboards/dashboard-data/insight-dashboard-line-score.multi.spec.ts
+++ eform-client/e2e/Tests/insight-dashboard-general/dashboards/dashboard-data/insight-dashboard-line-score.multi.spec.ts
@@ -9,6 +9,7 @@ import {
   dashboardLineScoreDataJson,
   dashboardLineScoreItems
 } from '../../../../Page objects/InsightDashboard/ChartData/DashboardLineScore.data';
+import { $ } from '@wdio/globals';
 
 const dashboardConfig: DashboardTestConfigEditModel = {
   locationTagName: 'Location 1',
